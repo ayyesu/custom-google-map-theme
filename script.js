94f207030de68eaf43640717bdbc9706d0def024
Daniel Kissiedu <iamdanielkissiedu@gmail.com>
2022-11-04 21:14:06 +0000

    Map loaded

---
+++ script.js
@@ -0,0 +1,7 @@
+const map_container = document.getElementById("map");
+
+const map = new google.maps.Map(map_container, {
+    center: {lat: 5.614818, lng: -0.205874},
+    zoom: 8,
+    mapId: '49cb5c1c66487d81'
+})
